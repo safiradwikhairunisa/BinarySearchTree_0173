2db5de9b8ea81d0653f244c47d25c78dfbe0b922
safiradwikhairunisa <safiradwikhairunisa@gmail.com>
2025-06-26 10:48:12 +0700

    add void postorder

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -123,4 +123,18 @@ class BinaryTree
         preorder(ptr->rightchild);
     }
 
+    void postorder(Node *ptr)
+    {
+        if (isEmpty())
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
+
+        postorder(ptr->leftchild);
+        postorder(ptr->rightchild);
+        cout << ptr->info << " "; //parent
+    }
 }
